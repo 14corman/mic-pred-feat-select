a6eb4ecb2f98af71c49bffd13e601d48bcb1a6df
14corman <cory-edwards@uiowa.edu>
2021-04-15 20:17:38 -0500

    Created Random Forest testing Jupyter Notebook and finalized python script.

--- script/forests/__init__.py
+++ script/forests/__init__.py
@@ -6,4 +6,5 @@ Created on Mar 24 6:55 PM 2021
 
 """
 
-from .gradient_forest import *
+from .gradient_forest import check_and_create_xgb_folders, GradientForest
+from .random_forest import check_and_create_rf_folders, RandomForest
